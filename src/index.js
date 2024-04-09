35ea4a3ecff621d9d5bcf7392dbdbe6096ac2082
ojpbraga <ojpbraga@gmail.com>
2024-04-09 00:59:00 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const app = express(); // Função que cria o app
+const PORT = 3000;
+
+// Quando ele receber um get (suponhamos, do formulário), tomará esta ação
+
+// req: ver informações de quem está acessando o site, tipo o ip do usuário / REQUISIÇÃO
+// res: resposta do backend para o usuário
+    // rota
+app.get('/', (req, res) => {
+    // Colocamos o que queremos mandar de volta para a requisição
+    res.send("Get"); // Resposta
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Rodando em: http://localhost:${PORT}`);
+})
+
+// !!! Não Atualiza Automaticamente, precisa usar o NodeMon
